6374a40b2906962bcdef58d8b106f25474f064cb
朱思嘉 <zhusijia@hotoa.com>
2018-03-18 20:30:39 +0800

    goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,44 +1,48 @@
 <template>
   <div class="goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" ref="menuWrapper">
 			<ul>
-				<li v-for="item in goods" class="menu-item">
+				<li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}">
 					<span class="text border-1px">
 						<span v-if="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
 				</li>
 			</ul>
 		</div>
-		<div class="goods-wrapper">
-			<div v-for="item in goods" class="food-list">
-				<h1 class="list-title">
-					{{item.name}}
-				</h1>
-				<ul>
-					<li v-for="food in item.foods" class="food-detail border-1px">
-						<div class="icon">
-							<img :src="food.icon" width="57" height="57">
-						</div>
-						<div class="cotent">
-							<h2 class="name">{{food.name}}</h2>
-							<p class="desc">{{food.description}}</p>
-							<div class="extra">
-								<span class="sell-count">月售{{food.sellCount}}</span>
-								<span class="rating">好评率{{food.rating}}%</span>
+		<div class="goods-wrapper" ref="goodsWrapper">
+			<div>
+				<div v-for="item in goods" class="food-list food-list-hook">
+					<h1 class="list-title">
+						{{item.name}}
+					</h1>
+					<ul>
+						<li v-for="food in item.foods" class="food-detail border-1px">
+							<div class="icon">
+								<img :src="food.icon" width="57" height="57">
 							</div>
-							<div class="price">
-								<span>{{food.price}}</span>
-								<span v-if="food.oldPrice">{{food.oldPrice}}</span>
+							<div class="cotent">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span class="sell-count">月售{{food.sellCount}}</span>
+									<span class="rating">好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span class="now">¥{{food.price}}</span>
+									<span v-if="food.oldPrice" class="old">¥{{food.oldPrice}}</span>
+								</div>
 							</div>
-						</div>
-					</li>
-				</ul>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
   </div>
 </template>
 
 <script>
+	import BScroll from 'better-scroll';
+
 	const ERR_OK = 0;
 	export default {
 		name: 'v-goods',
@@ -50,17 +54,57 @@
 		data () {
 			return {
 				goods: {},
-				classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+				listHeight: [],
+				scrollY: 0,
+				cIndex: 0
 			}
 		},
 		created() {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 			this.$http.get('/api/goods').then(response => {
 				response = response.body;
 				if(response.errno === ERR_OK){
 					this.goods = response.data;
+					this.$nextTick(()=> {
+						this._initScroll();
+						this._calculateHeight();
+					})
 				}
-				console.log(this.goods)
 			})
+		},
+		computed: {
+		 currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      }
+		},
+		methods: {
+			_initScroll() {
+				this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+				this.goodsScroll = new BScroll(this.$refs.goodsWrapper, {
+					probeType: 3
+				});
+				this.goodsScroll.on('scroll', (pos)=>{
+					this.scrollY = Math.abs(Math.round(pos.y));
+				})
+			},
+			_calculateHeight() {
+				let goodsList = this.$refs.goodsWrapper.getElementsByClassName("food-list-hook");
+				let height = 0;
+				this.listHeight.push(height);
+				for(let i = 0;i < goodsList.length; i++ ) {
+					let item = goodsList[i]
+					height += item.clientHeight;
+					this.listHeight.push(height);
+				}
+				console.log("this.listHeight", this.listHeight)
+			}
 		}
 	}
 </script>
@@ -84,6 +128,14 @@
 				width 56px
 				line-height 14px
 				padding 0 12px
+				&.current
+					position relative
+					background-color #fff;
+					margin-top -1px
+					z-index 10
+					font-weight 700
+					.text
+						border-none()
 				.text
 					display table-cell
 					width 56px
@@ -139,7 +191,7 @@
 						margin-top 2px
 					.desc
 						font-size 10px
-						line-height 10px
+						line-height 12px
 						color rgb(147, 153, 159)
 						margin-top 8px
 					.extra
@@ -149,7 +201,20 @@
 						.sell-count
 							font-size 10px
 							line-height 10px
+							margin-right: 12px;
 						.rating
 							font-size 10px
 							line-height 10px
+					.price
+						font-size 24px
+						.now
+							margin-right 8px
+							font-size 14px
+							font-weight 700
+							color rgb(240, 20, 20)
+						.old
+							text-decoration line-through
+							font-size 10px
+							font-weight 700
+							color rgb(147, 153, 159)
 </style>
